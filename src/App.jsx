42b5069edb7a35fb6fec78bd162afee50e1f1314
Vu Minh Thien <vthien2k33@gmail.com>
2025-07-08 20:03:51 +0700

    theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Button from "@mui/material/Button";
 import { pink } from "@mui/material/colors";
 import HomeIcon from "@mui/icons-material/Home";
+import { Typography } from "@mui/material";
 function App() {
   return (
     <>
@@ -12,6 +13,9 @@ function App() {
       <HomeIcon color="disabled" />
       <HomeIcon sx={{ color: pink[500] }} />
       <div>Minh Thien Dev</div>
+      <Typography variant="body2" color="text.secondary">
+        Test
+      </Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
+import { ThemeProvider } from "@emotion/react";
 import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./theming.js";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
