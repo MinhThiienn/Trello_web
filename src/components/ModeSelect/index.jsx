a81e55c2d0108066f35519c28d7b83424bfd82b1
Vu Minh Thien <vthien2k33@gmail.com>
2025-07-23 17:21:59 +0700

    style

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -7,6 +7,7 @@ import Select from "@mui/material/Select";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import { colors } from "@mui/material";
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme();
@@ -27,13 +28,27 @@ function ModeSelect() {
         onChange={handleChange}
       >
         <MenuItem value="light">
-          <Box style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: "8px",
+              color: "primary.main",
+            }}
+          >
             {" "}
             <LightModeIcon fontSize="small" /> Light
           </Box>
         </MenuItem>
         <MenuItem value="dark">
-          <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: "8px",
+              color: "primary.main",
+            }}
+          >
             {" "}
             <DarkModeIcon fontSize="small" /> Dark
           </Box>
