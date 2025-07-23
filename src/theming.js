31eac202b6b27f5ad5646b3afd29afe99c29ee7c
Vu Minh Thien <vthien2k33@gmail.com>
2025-07-23 16:15:53 +0700

    Resolve merge conflict in theming.js

--- src/theming.js
+++ src/theming.js
@@ -24,7 +24,6 @@ const theme = extendTheme({
       },
     },
   },
-<<<<<<< HEAD
 
   components: {
     MuiCssBaseline: {
@@ -48,19 +47,13 @@ const theme = extendTheme({
         },
       },
     },
-=======
-  components: {
->>>>>>> 2764f4c966c245dcdf0f4c2a87ea662f456196ae
+
     MuiButton: {
       styleOverrides: {
         root: {
           textTransform: "none",
           fontWeight: "bold",
-<<<<<<< HEAD
-          borderRadius: "4px",
-=======
           borderRadius: "4px", // Custom border radius
->>>>>>> 2764f4c966c245dcdf0f4c2a87ea662f456196ae
         },
       },
     },
@@ -68,14 +61,7 @@ const theme = extendTheme({
       styleOverrides: {
         root: ({ theme }) => ({
           color: theme.palette.primary.main,
-<<<<<<< HEAD
-          fontSize: "0.875rem",
-=======
           fontSize: "0.875rem", // 14px
-          // "&.Mui-focused": {
-          //   color: theme.palette.primary.main,
-          // },
->>>>>>> 2764f4c966c245dcdf0f4c2a87ea662f456196ae
         }),
       },
     },
@@ -84,11 +70,7 @@ const theme = extendTheme({
         root: ({ theme }) => {
           return {
             color: theme.palette.text.primary,
-<<<<<<< HEAD
-            fontSize: "0.875rem",
-=======
             fontSize: "0.875rem", // 14px
->>>>>>> 2764f4c966c245dcdf0f4c2a87ea662f456196ae
             "& .MuiOutlinedInput-notchedOutline": {
               borderColor: theme.palette.primary.light,
             },
